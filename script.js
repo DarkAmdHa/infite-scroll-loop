2261b985bc7903dd025fa29ac72c45045ed9539c
Hamid <59122894+DarkAmdHa@users.noreply.github.com>
2021-11-13 22:57:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+var doc = window.document,
+  context = doc.querySelector(".names-flex"),
+  items = doc.querySelectorAll(".name-item"),
+  h = 0,
+  clones = [],
+  disableScroll = false,
+  scrollHeight = 0,
+  scrollPos = 0,
+  clonesHeight = 0;
+
+items.forEach((item) => {
+  //Getting the initial height before cloning
+  let itemH = item.getBoundingClientRect().height;
+  itemH += parseInt(
+    window.getComputedStyle(item).marginBottom +
+      window.getComputedStyle(item).marginTop +
+      window.getComputedStyle(item).paddingBottom +
+      window.getComputedStyle(item).paddingTop
+  ); //Getting the margins and paddings(top and bottom only)
+  h = h + itemH;
+});
+
+function getScrollPos() {
+  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
+}
+
+function setScrollPos(pos) {
+  context.scrollTop = pos;
+}
+
+function getClonesHeight() {
+  clonesHeight = 0;
+
+  Array.from(clones, (clone) => {
+    clonesHeight = clonesHeight + clone.offsetHeight;
+  });
+
+  return clonesHeight;
+}
+
+function reCalc() {
+  scrollPos = getScrollPos();
+  scrollHeight = context.scrollHeight;
+  clonesHeight = getClonesHeight();
+
+  if (scrollPos <= 0) {
+    setScrollPos(2); // Scroll 1 pixel to allow upwards scrolling
+  }
+}
+
+function scrollUpdate() {
+  if (!disableScroll) {
+    scrollPos = getScrollPos();
+
+    if (scrollPos + innerHeight >= scrollHeight) {
+      // Scroll to the top(of the cloned ) when you’ve reached the bottom
+      console.log(innerHeight);
+      console.log(context.scrollTop);
+      setScrollPos(h - innerHeight); // Scroll to
+      disableScroll = true;
+    } else if (scrollPos <= 1) {
+      // Scroll to the bottom when you reach the top
+
+      setScrollPos(h * 3);
+      disableScroll = true;
+    }
+
+    if (disableScroll) {
+      // Disable scroll-jumping for a short time to avoid flickering
+      window.setTimeout(function () {
+        disableScroll = false;
+      }, 20);
+    }
+  }
+}
+
+function onLoad() {
+  Array.from(items, (item, index) => {
+    const clone = item.cloneNode(true);
+    context.appendChild(clone);
+    clone.classList.add("js-clone");
+  });
+
+  Array.from(items, (item, index) => {
+    const clone = item.cloneNode(true);
+    context.appendChild(clone);
+    clone.classList.add("js-clone");
+  });
+
+  Array.from(items, (item, index) => {
+    const clone = item.cloneNode(true);
+    context.appendChild(clone);
+    clone.classList.add("js-clone");
+  });
+
+  clones = context.querySelectorAll(".js-clone");
+  setScrollPos(2);
+
+  reCalc();
+
+  context.addEventListener(
+    "touchmove",
+    function () {
+      window.requestAnimationFrame(scrollUpdate);
+    },
+    false
+  );
+
+  context.addEventListener(
+    "scroll",
+    function () {
+      window.requestAnimationFrame(scrollUpdate);
+    },
+    false
+  );
+
+  window.addEventListener(
+    "resize",
+    function () {
+      window.requestAnimationFrame(reCalc);
+    },
+    false
+  );
+}
+
+window.onload = onLoad;
