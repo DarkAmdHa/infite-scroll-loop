ff314e24a8016431b526b03d251b17d5d83826f5
Hamid <59122894+DarkAmdHa@users.noreply.github.com>
2021-11-14 17:18:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-var doc = window.document,
-  context = doc.querySelector(".names-flex"),
-  items = doc.querySelectorAll(".name-item"),
-  h = 0,
-  clones = [],
-  disableScroll = false,
-  scrollHeight = 0,
-  scrollPos = 0,
-  clonesHeight = 0;
-items.forEach((item) => {
+var h = 0;
+document.querySelectorAll(".name-item").forEach((item) => {
   //Getting the initial height before cloning
   let itemH = item.getBoundingClientRect().height;
   itemH += parseInt(
@@ -18,130 +10,72 @@ items.forEach((item) => {
   ); //Getting the margins and paddings(top and bottom only)
   h = h + itemH;
 });
+const loop = [];
+let state;
+const scr = (_) => document.querySelector(".loop-container").scrollTop;
+document.addEventListener("DOMContentLoaded", (_) => {
+  loop.push(document.querySelector(".loop"));
+  loop.push(loop[0].cloneNode(true));
 
-function getScrollPos() {
-  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
-}
-
-function setScrollPos(pos) {
-  context.scrollTop = pos;
-}
-
-function getClonesHeight() {
-  clonesHeight = 0;
-
-  Array.from(clones, (clone) => {
-    clonesHeight = clonesHeight + clone.offsetHeight;
+  const wrapper = loop[0].parentNode;
+  const height = loop[0].scrollHeight;
+  loop.forEach((l, i) => {
+    l.style.height = `${height}px`;
+    l.style.top = `${height * i}px`;
   });
+  wrapper.appendChild(loop[1]);
 
-  return clonesHeight;
-}
+  const currLoop = Symbol("loop");
+  state = {
+    [currLoop]: 0,
+    get otherLoop() {
+      return loop[(state[currLoop] = (this[currLoop] + 1) % 2)];
+    },
+    get loopScroll() {
+      const cE = this.currElement;
+      return cE.scrollHeight + cE.offsetTop;
+    },
+    get currElement() {
+      return loop[this[currLoop]];
+    },
+    get currIndex() {
+      return this[currLoop];
+    },
+    get goingDown() {
+      return state.previous <= scr();
+    },
+    renderNext(scrollAmt) {
+      console.log("Render next loop", { current: scr(), target: scrollAmt });
+      const cLoop = state.otherLoop;
+      cLoop.style.top = `${scrollAmt}px`;
+      document.querySelector(".loop-wrapper").style.height = `${
+        scrollAmt + height * 2
+      }px`;
+    },
+    renderPrev(scrollAmt) {
+      console.log("Render prev loop", { current: scr(), target: scrollAmt });
+      const cLoop = state.otherLoop;
+      cLoop.style.top = `${scrollAmt - 2 * height}px`;
+      document.querySelector(".loop-wrapper").style.height = `${
+        scrollAmt + height * 2
+      }px`;
+    },
+  };
+  document.querySelector(".loop-container").scrollTop = 1;
+  let disableScroll = false;
 
-function reCalc() {
-  if (scrollPos <= 0) {
-    setScrollPos(2); // Scroll 1 pixel to allow upwards scrolling
-  }
-  console.log("recalculating");
-  scrollPos = getScrollPos();
-  scrollHeight = context.scrollHeight;
-  clonesHeight = getClonesHeight();
-}
+  document.querySelector(".loop-container").addEventListener("scroll", (e) => {
+    const scrollAmt = state.loopScroll;
 
-function scrollUpdate() {
-  if (!disableScroll) {
-    scrollPos = getScrollPos();
-    if (scrollPos + innerHeight >= scrollHeight - 5) {
-      // Scroll to the top(of the cloned ) when you’ve reached the bottom
-      // context.style.overflowY = "hidden";
-      var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-      if (isSafari) {
-        s = scrollPos;
-        setTimeout(() => {
-          setScrollPos(s - h); // Scroll to
-          disableScroll = true;
-          context.style.overflowY = "scroll";
-
-          if (disableScroll) {
-            // Disable scroll-jumping for a short time to avoid flickering
-            window.setTimeout(function () {
-              disableScroll = false;
-            }, 40);
-          }
-        }, 1500);
-      } else {
-        setScrollPos(context.scrollTop - h); // Scroll to
-        disableScroll = true;
-      }
-
-      // setTimeout(() => {
-      //   context.style.overflowY = "scroll";
-      // }, 1);
-    } else if (scrollPos <= 1) {
-      // Scroll to the bottom when you reach the top
-      // context.style.overflowY = "hidden";
-
-      setScrollPos(h);
-      disableScroll = true;
-
-      // setTimeout(() => {
-      //   context.style.overflowY = "scroll";
-      // }, 1);
+    console.log({ current: scr(), target: scrollAmt });
+    if (document.querySelector(".loop-container").scrollTop <= 0) {
+      document.querySelector(".loop-container").scrollTop = h;
+      state.renderNext(h);
     }
-  }
-  if (disableScroll) {
-    // Disable scroll-jumping for a short time to avoid flickering
-    window.setTimeout(function () {
-      disableScroll = false;
-    }, 40);
-  }
-}
 
-function onLoad() {
-  Array.from(items, (item, index) => {
-    const clone = item.cloneNode(true);
-    context.appendChild(clone);
-    clone.classList.add("js-clone");
+    if (state.goingDown && scr() > scrollAmt - h) state.renderNext(scrollAmt);
+    else if (!state.goingDown && scr() < scrollAmt - 0.9 * height)
+      state.renderPrev(scrollAmt);
+    state.previous = scr();
   });
-
-  Array.from(items, (item, index) => {
-    const clone = item.cloneNode(true);
-    context.appendChild(clone);
-    clone.classList.add("js-clone");
-  });
-
-  Array.from(items, (item, index) => {
-    const clone = item.cloneNode(true);
-    context.appendChild(clone);
-    clone.classList.add("js-clone");
-  });
-
-  clones = context.querySelectorAll(".js-clone");
-  setScrollPos(2);
-  reCalc();
-
-  context.addEventListener(
-    "touchmove",
-    function () {
-      window.requestAnimationFrame(scrollUpdate);
-    },
-    false
-  );
-
-  context.addEventListener(
-    "scroll",
-    function () {
-      window.requestAnimationFrame(scrollUpdate);
-    },
-    false
-  );
-
-  window.addEventListener(
-    "resize",
-    function () {
-      window.requestAnimationFrame(reCalc);
-    },
-    false
-  );
-}
-
-window.onload = onLoad;
+});
