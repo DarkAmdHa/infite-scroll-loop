8ae3800c9feaf8d2a8f8158bb247d9d5d7c87055
Hamid <59122894+DarkAmdHa@users.noreply.github.com>
2021-11-14 05:01:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,119 @@
-html,
-body {
-  height: 100%;
-  width: 100%;
-  background: #fff;
-  position: fixed;
+var doc = window.document,
+  context = doc.querySelector(".names-flex"),
+  items = doc.querySelectorAll(".name-item"),
+  h = 0,
+  clones = [],
+  disableScroll = false,
+  scrollHeight = 0,
+  scrollPos = 0,
+  clonesHeight = 0;
+items.forEach((item) => {
+  //Getting the initial height before cloning
+  let itemH = item.getBoundingClientRect().height;
+  itemH += parseInt(
+    window.getComputedStyle(item).marginBottom +
+      window.getComputedStyle(item).marginTop +
+      window.getComputedStyle(item).paddingBottom +
+      window.getComputedStyle(item).paddingTop
+  ); //Getting the margins and paddings(top and bottom only)
+  h = h + itemH;
+});
+
+function getScrollPos() {
+  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
 }
 
-.names-flex {
-  position: relative;
-  height: 100%;
-  overflow-x: hidden;
-  overflow-y: scroll;
-  -webkit-overflow-scrolling: touch;
-  width: 100%;
+function setScrollPos(pos) {
+  context.scrollTop = pos;
 }
 
-.names-flex::-webkit-scrollbar {
-  display: none;
+function getClonesHeight() {
+  clonesHeight = 0;
+
+  Array.from(clones, (clone) => {
+    clonesHeight = clonesHeight + clone.offsetHeight;
+  });
+
+  return clonesHeight;
 }
 
-/* Hide scrollbar for IE, Edge and Firefox */
-.names-flex {
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
+function reCalc() {
+  if (scrollPos <= 0) {
+    setScrollPos(2); // Scroll 1 pixel to allow upwards scrolling
+  }
+  console.log("recalculating");
+  scrollPos = getScrollPos();
+  scrollHeight = context.scrollHeight;
+  clonesHeight = getClonesHeight();
 }
 
-.names-flex.names-content {
-  padding-top: 0;
-  padding-bottom: 0;
+function scrollUpdate() {
+  if (!disableScroll) {
+    scrollPos = getScrollPos();
+
+    if (scrollPos + innerHeight >= scrollHeight - 3) {
+      // Scroll to the top(of the cloned ) when you’ve reached the bottom
+      setScrollPos(context.scrollTop - h); // Scroll to
+    } else if (scrollPos <= 1) {
+      // Scroll to the bottom when you reach the top
+      setScrollPos(h);
+    }
+
+    if (disableScroll) {
+      // Disable scroll-jumping for a short time to avoid flickering
+      window.setTimeout(function () {
+        disableScroll = false;
+      }, 40);
+    }
+  }
 }
-.section.content.wf-section {
-  z-index: 3;
-  height: 100% !important;
+
+function onLoad() {
+  Array.from(items, (item, index) => {
+    const clone = item.cloneNode(true);
+    context.appendChild(clone);
+    clone.classList.add("js-clone");
+  });
+
+  Array.from(items, (item, index) => {
+    const clone = item.cloneNode(true);
+    context.appendChild(clone);
+    clone.classList.add("js-clone");
+  });
+
+  Array.from(items, (item, index) => {
+    const clone = item.cloneNode(true);
+    context.appendChild(clone);
+    clone.classList.add("js-clone");
+  });
+
+  clones = context.querySelectorAll(".js-clone");
+  setScrollPos(2);
+  reCalc();
+
+  context.addEventListener(
+    "touchmove",
+    function () {
+      window.requestAnimationFrame(scrollUpdate);
+    },
+    false
+  );
+
+  context.addEventListener(
+    "scroll",
+    function () {
+      window.requestAnimationFrame(scrollUpdate);
+    },
+    false
+  );
+
+  window.addEventListener(
+    "resize",
+    function () {
+      window.requestAnimationFrame(reCalc);
+    },
+    false
+  );
 }
+
+window.onload = onLoad;
